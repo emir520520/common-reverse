1d3a5b84427c649cf9b5bf553297a5026558bf3a
emir520520 <frankfang111@gmail.com>
2022-08-24 21:17:37 -0400

    Aug 24, 2022

--- src/main/java/ca/fangyux/adminapp/entity/Role.java
+++ src/main/java/ca/fangyux/adminapp/entity/Role.java
@@ -1,15 +1,28 @@
 package ca.fangyux.adminapp.entity;
 
 public class Role {
-    private Long id;
+    private Integer id;
 
     private String name;
 
-    public Long getId() {
+    public Role(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
